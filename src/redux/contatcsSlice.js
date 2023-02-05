4d071b909c606d090cb2f1b7968894bc5688f698
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-02-05 21:52:01 +0200

    optimizied contactsSlice

--- src/redux/contatcsSlice.js
+++ src/redux/contatcsSlice.js
@@ -1,6 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, addContacts, deleteContacts } from './operations';
 
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
@@ -9,33 +18,23 @@ const contactsSlice = createSlice({
     error: null,
   },
   extraReducers: {
-    [fetchContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchContacts.pending]: handlePending,
     [fetchContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items = action.payload;
     },
-    [fetchContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [addContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchContacts.rejected]: handleRejected,
+
+    [addContacts.pending]: handlePending,
     [addContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items.push(action.payload);
     },
-    [addContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [deleteContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [addContacts.rejected]: handleRejected,
+
+    [deleteContacts.pending]: handlePending,
     [deleteContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
@@ -45,10 +44,7 @@ const contactsSlice = createSlice({
       );
       state.items.splice(index, 1);
     },
-    [deleteContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+    [deleteContacts.rejected]: handleRejected,
   },
 });
 
